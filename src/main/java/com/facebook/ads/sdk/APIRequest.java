d3779d6b3e6b092114d7332c5b640702f79b947c
Andrey <andrey.makarich@gmail.com>
2018-08-21 19:45:18 +0300

    handle null error stream

--- src/main/java/com/facebook/ads/sdk/APIRequest.java
+++ src/main/java/com/facebook/ads/sdk/APIRequest.java
@@ -22,6 +22,7 @@
  */
 package com.facebook.ads.sdk;
 
+import java.io.*;
 import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -32,14 +33,9 @@ import java.net.URL;
 import java.net.URLEncoder;
 import javax.net.ssl.HttpsURLConnection;
 import org.omg.CORBA.Request;
-import java.io.BufferedReader;
+
 import java.lang.reflect.Modifier;
 import java.lang.StringBuilder;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.IOException;
 import java.util.Random;
 
 import com.google.common.base.Function;
@@ -283,27 +279,28 @@ public class APIRequest<T extends APINode> {
   }
 
   private static String readResponse(HttpsURLConnection con) throws APIException, IOException {
-    try {
-      int responseCode = con.getResponseCode();
-
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      String inputLine;
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
       StringBuilder response = new StringBuilder();
 
+      String inputLine;
       while ((inputLine = in.readLine()) != null) {
         response.append(inputLine);
       }
-      in.close();
       return response.toString();
     } catch(Exception e) {
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-      String inputLine;
       StringBuilder response = new StringBuilder();
+      InputStream errorStream = con.getErrorStream();
 
-      while ((inputLine = in.readLine()) != null) {
-        response.append(inputLine);
+      if (errorStream != null) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(errorStream))) {
+          String inputLine;
+          while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+          }
+        }
+      } else {
+        response.append("{ \"message\" : \"Connection closed\" }");
       }
-      in.close();
       throw new APIException.FailedRequestException(response.toString(), e);
     }
   }
